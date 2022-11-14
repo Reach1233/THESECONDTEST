a22c6030be9f7f0094a9458a15671bee7b33e4a8
Reach <111524463+Reach1233@users.noreply.github.com>
2022-11-14 13:09:43 -0500

    Add files via upload

---
+++ mommymilkers.js
@@ -0,0 +1,25 @@
+//Side Menu
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+var x = 0;
+function toggleNav(){
+	if (x == 0){
+		openNav();
+		x++;
+	}else{
+		closeNav();
+		x--;
+	}
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+//Calnder
+/*$("[.fc-daygrid-event][.fc-daygrid-block-event][.fc-h-event][.fc-event][.fc-event-start][.fc-event-end][.fc-event-past]").sortable({
+	connectWith: "[.fc-daygrid-day] [.fc-day] [.fc-day-wed] [.fc-day-past]"
+});*/
+//Chess
